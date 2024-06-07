ec538becbb14b7f97c432ad23816d637ee38cb5a
Sachin <sachinkshuklaoo7@gmail.com>
2024-06-07 11:53:53 +0530

    Model create

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from database.models import celeb
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kahani.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+def init_db():
+    with app.app_context():
+        db.create_all()
+        print("Database initialized")
+
 if __name__ == '__main__':
+    init_db()
     app.run(host="0.0.0.0", port=8080, debug=True)
--- database/db.py
+++
@@ -1,9 +0,0 @@
-from flask import app
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy(app)
-
-# Init DB
-@app.cli.command('initdb')
-def init_db():
-    db.create_all()
