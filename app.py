b64451e00882ba7c347233ae8f52d69a38205fe1
Sachin <sachinkshuklaoo7@gmail.com>
2024-06-13 08:33:57 +0530

    fix db refactor issue

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import traceback
 from flask import Flask, request, jsonify
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
 from controllers.scrape import Scrape
 from controllers.search import Search
+from database.models import Celeb, Movie, db
 from utils.api import APIUtils
 from utils.contants import SearchItemType
 
@@ -11,59 +11,59 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kahani.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
-db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 # Models
-class Celeb(db.Model):
-    id = db.Column(db.String(10), primary_key=True)
-    name = db.Column(db.String(200), nullable=False)
-    image = db.Column(db.String(200))
+# class Celeb(db.Model):
+#     id = db.Column(db.String(10), primary_key=True)
+#     name = db.Column(db.String(200), nullable=False)
+#     image = db.Column(db.String(200))
 
-    def store_celeb(self, data):
-        if data["type"] != "Celeb":
-            return None
+#     def store_celeb(self, data):
+#         if data["type"] != "Celeb":
+#             return None
 
-        celeb = Celeb.query.filter_by(id=data["id"]).first()
-        if not celeb:
-            celeb = Celeb(id=data["id"], name=data["name"], image=data["image"])
-            db.session.add(celeb)
+#         celeb = Celeb.query.filter_by(id=data["id"]).first()
+#         if not celeb:
+#             celeb = Celeb(id=data["id"], name=data["name"], image=data["image"])
+#             db.session.add(celeb)
 
-        try:
-            db.session.commit()
-            print(f"Stored celeb: {celeb}")
-            return celeb
-        except Exception as e:
-            print(f"Error storing celeb: {e}")
-            return None
+#         try:
+#             db.session.commit()
+#             print(f"Stored celeb: {celeb}")
+#             return celeb
+#         except Exception as e:
+#             print(f"Error storing celeb: {e}")
+#             return None
 
 # Movie Model
-class Movie(db.Model):
-    id = db.Column(db.String(10), primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    year = db.Column(db.Integer)
-    type = db.Column(db.String(10))
-    rating = db.Column(db.Float)
+# class Movie(db.Model):
+#     id = db.Column(db.String(10), primary_key=True)
+#     title = db.Column(db.String(80), nullable=False)
+#     year = db.Column(db.Integer)
+#     type = db.Column(db.String(10))
+#     rating = db.Column(db.Float)
 
-    def store_movie(self, data):
-        if data["type"] != SearchItemType.MOVIE.value and data["type"] != SearchItemType.TV_SHOW.value:
-            return None
+#     def store_movie(self, data):
+#         if data["type"] != SearchItemType.MOVIE.value and data["type"] != SearchItemType.TV_SHOW.value:
+#             return None
 
-        movie = Movie.query.filter_by(id=data["id"]).first()
-        if not movie:
-            movie = Movie(id=data["id"], title=data["name"], year=data["year"], type=data["type"])
-            db.session.add(movie)
-            db.session.commit()
-            print(f"Stored movie: {movie}")
+#         movie = Movie.query.filter_by(id=data["id"]).first()
+#         if not movie:
+#             movie = Movie(id=data["id"], title=data["name"], year=data["year"], type=data["type"])
+#             db.session.add(movie)
+#             db.session.commit()
+#             print(f"Stored movie: {movie}")
 
-        try:
-            db.session.commit()
-            print(f"Stored movie: {movie}")
-            return movie
-        except Exception as e:
-            print(f"Error storing movie: {e}")
-            return None
+#         try:
+#             db.session.commit()
+#             print(f"Stored movie: {movie}")
+#             return movie
+#         except Exception as e:
+#             print(f"Error storing movie: {e}")
+#             return None
 
 @app.route('/search', methods=['POST'])
 def search():
---
+++ database/models.py
@@ -0,0 +1,53 @@
+from flask_sqlalchemy import SQLAlchemy
+from utils.contants import SearchItemType
+
+db = SQLAlchemy()
+
+class Celeb(db.Model):
+    id = db.Column(db.String(10), primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    image = db.Column(db.String(200))
+
+    def store_celeb(self, data):
+        if data["type"] != "Celeb":
+            return None
+
+        celeb = Celeb.query.filter_by(id=data["id"]).first()
+        if not celeb:
+            celeb = Celeb(id=data["id"], name=data["name"], image=data["image"])
+            db.session.add(celeb)
+
+        try:
+            db.session.commit()
+            print(f"Stored celeb: {celeb}")
+            return celeb
+        except Exception as e:
+            print(f"Error storing celeb: {e}")
+            return None
+
+class Movie(db.Model):
+    id = db.Column(db.String(10), primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    year = db.Column(db.Integer)
+    type = db.Column(db.String(10))
+    rating = db.Column(db.Float)
+
+    def store_movie(self, data):
+        if data["type"] != SearchItemType.MOVIE.value and data["type"] != SearchItemType.TV_SHOW.value:
+            return None
+
+        movie = Movie.query.filter_by(id=data["id"]).first()
+        if not movie:
+            movie = Movie(id=data["id"], title=data["name"], year=data["year"], type=data["type"])
+            db.session.add(movie)
+            db.session.commit()
+            print(f"Stored movie: {movie}")
+
+        try:
+            db.session.commit()
+            print(f"Stored movie: {movie}")
+            return movie
+        except Exception as e:
+            print(f"Error storing movie: {e}")
+            return None
+
